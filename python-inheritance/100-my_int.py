a2402a67e5ab5fab69feb4ccafbaa9c3c2e430d5
Ajok Atem Biar <a.biar@alustudent.com>
2023-07-28 04:47:33 -0700

    task

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-class MyInt(int):
-    def __eq__(self, other):
-        """Override the equality operator (==)"""
-        return super().__ne__(other)
+"""Defines a class MyInt that inherits from int."""
 
-    def __ne__(self, other):
-        """Override the not equal operator (!=)"""
-        return super().__eq__(other)
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
