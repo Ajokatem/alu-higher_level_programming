485c4332fe59dd1be20950e68250d3acc8e44306
Ajok Atem Biar <a.biar@alustudent.com>
2023-07-27 22:52:45 -0700

    0008

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-""" module subclass rectangle
-"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+'''Defines function class_to_json
+'''
 
 
-class Rectangle(BaseGeometry):
-    """Rectangle that inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        BaseGeometry.integer_validator(self, 'height', height)
-        self.__height = height
-        BaseGeometry.integer_validator(self, 'width', width)
-        self.__width = width
+def class_to_json(obj):
+    '''Writes the dictionary description for json serialization of an object
+    '''
+    return (obj.__dict__)
