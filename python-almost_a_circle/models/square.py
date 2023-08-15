56262b276ecdf2094921cc43885e40f341005426
Ajok Atem Biar <a.biar@alustudent.com>
2023-08-15 07:21:18 -0700

    taskkk

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module
+"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """
+    class Square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        obj
+        """
+        super().__init__(self, x, y, id)
