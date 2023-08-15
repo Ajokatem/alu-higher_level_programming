b256055936d2643a06b04d32f20ab38692c771bf
Ajok Atem Biar <a.biar@alustudent.com>
2023-08-15 06:00:16 -0700

    task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -31,6 +31,10 @@ class Rectangle(Base):
         """
         doc
         """
+        if not isinstance(width, int):
+            raise TypeError("height must be an integer")
+        if width <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -45,6 +49,10 @@ class Rectangle(Base):
         """
         doc
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -59,6 +67,8 @@ class Rectangle(Base):
         """
         doc
         """
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -73,4 +83,6 @@ class Rectangle(Base):
         """
         doc
         """
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
