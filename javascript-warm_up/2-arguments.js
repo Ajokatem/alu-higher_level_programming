f100e0f8092f3c51c27336c34d24fe084eb657c8
Ajokatem <a.bair@alustudent.com>
2023-09-29 23:04:07 +0000

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
+const numberOfArgs = process.argv.length - 2;
 
-if (args.length === 0) {
+if (numberOfArgs === 0) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (numberOfArgs === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
