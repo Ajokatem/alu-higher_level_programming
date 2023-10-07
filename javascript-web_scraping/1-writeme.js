e7e382dfbd987e3a4a9d30832b86e6d7ee3d412d
Ajokatem <a.bair@alustudent.com>
2023-10-07 21:49:20 +0000

    task1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-//PRINT ERROR
-fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
-  if (err) {
-    console.log(err);
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFileSync(filePath, stringToWrite, 'utf-8', function (error) {
+  if (error) {
+    console.error(error);
   }
 });
